08a21ee4a23818f2d40a4d2cfef34df32330db4e
Pawan <pawanb78@gmail.com>
2024-03-12 23:32:14 +0000

    book model created and working perfectly

--- src/Components/Home/BookSingleCard.jsx
+++ src/Components/Home/BookSingleCard.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {PiBookOpenTextLight} from 'react-icons/pi'
-import {BiUserCircle} from 'react-icons/bi'
+import {BiUserCircle, BiShow} from 'react-icons/bi'
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs'
 import {MdOutlineDelete} from 'react-icons/md'
+import { useState } from 'react';
+import BookModel from './BookModel';
 
 const BookSingleCard = ({books}) => {
+    const [showModel, setShowModel] = useState(false);
+
     return (
       <div key={books._id} className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'>
         <h2 className='absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg'>
@@ -22,6 +26,7 @@ const BookSingleCard = ({books}) => {
           <h2 className='my-1'>{books.author}</h2>
         </div>
         <div className='flex justify-between items-center gap-x-2 mt-4 p-4'>
+            <BiShow className='text-3xl text-blue-800 hover:text-black cursor-pointer' onClick={() => setShowModel(true)}/>
           <Link to={`/books/details/${books._id}`}>
             <BsInfoCircle className='text-2xl text-green-800 hover:text-black'/>
           </Link>
@@ -32,6 +37,7 @@ const BookSingleCard = ({books}) => {
             <MdOutlineDelete className='text-2xl text-red-800 hover:text-black'/>
           </Link>
         </div>
+        {showModel && (<BookModel books={books} onClose={() => setShowModel(false)}/>  ) }
       </div>
     );
   }
