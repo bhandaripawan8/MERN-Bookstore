330242d64deb0c85b0f06761c5eed94a26708c2f
Pawan <pawanb78@gmail.com>
2024-03-12 22:55:45 +0000

    table and card option at the home page, card component is working perfectly

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,14 @@ import {Link} from 'react-router-dom';
 import {AiOutlineEdit} from 'react-icons/ai';
 import {BsInfoCircle} from 'react-icons/bs';
 import {MdOutlineAddBox, MdOutlineDelete} from 'react-icons/md'
+import BooksTable from '../Components/Home/BooksTable';
+import BooksCard from '../Components/Home/BooksCard';
 
 const Home = () => {
     const [books, setBooks] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [showType, setShowType] = useState('table');
+
     useEffect(()=>{
       setLoading(true);
       axios.get('http://localhost:3000/api/books')
@@ -21,53 +25,30 @@ const Home = () => {
         setLoading(false);
       })
     },[])
-  return (
-    <div className='p-4'>
-<div className='p-4 flex justify-between items-center'>
-  <h1 className='text-3xl my-8'>Books List</h1>
-  <Link to={'/books/create'}>
-    <MdOutlineAddBox className='text-4xl text-blue-600 hover:text-blue-800'/>
-  </Link>
-</div>
-  {loading ? (<Spinner/>) : (
-    <table className='w-full border-separate border-spacing'>
-      <thead>
-        <tr>
-          <th className='border border-slate-600 rounded-md'>No</th>
-          <th className='border border-slate-600 rounded-md'>Title</th>
-          <th className='border border-slate-600 rounded-md max-md:hidden'>Author</th>
-          <th className='border border-slate-600 rounded-md max-md:hidden'>Published Year</th>
-          <th className='border border-slate-600 rounded-md'>Operations</th>
-        </tr>
-      </thead>
-      <tbody>
-        {books.map((book, index) => (
-          <tr className='h-8' key={book._id}>
-            <td className='border border-slate-700 rounded-md text-center'>{index + 1 }</td>
-            <td className='border border-slate-700 rounded-md text-center'>{book.title}</td>
-            <td className='border border-slate-700 rounded-md text-center max-md:hidden'>{book.author}</td>
-            <td className='border border-slate-700 rounded-md text-center max-md:hidden'>{book.publishedYear}</td>
-            <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
-              <div className='flex justify-center gap-x-4'>
-                <Link to={`/books/details/${book._id}`}>
-                  <BsInfoCircle className='text-2xl text-green-800'/>
-                </Link>
-                <Link to={`/books/edit/${book._id}`}>
-                  <AiOutlineEdit className='text-2xl text-yellow-600'/>
-                </Link>
-                <Link to={`/books/delete/${book._id}`}>
-                  <MdOutlineDelete className='text-2xl text-red-600'/>
-                </Link>
-              </div>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  )}
-</div>
-
-  )
-}
+    return (
+      <div className='p-4'>
+        <div className='flex justify-center items-center gap-x-4'>
+          <button className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg' onClick={() => setShowType('table')}>Table</button>
+          <button className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg' onClick={() => setShowType('card')}>Card</button>
+        </div>
+        <div className='p-4 flex justify-between items-center'>
+          <h1 className='text-3xl my-8'>Books List</h1>
+          <Link to={'/books/create'}>
+            <MdOutlineAddBox className='text-4xl text-blue-600 hover:text-blue-800'/>
+          </Link>
+        </div>
+        <div>
+          {loading ? (
+            <Spinner />
+          ) : showType === 'table' ? (
+            <BooksTable books={books} />
+          ) : (
+            <BooksCard books={books} />
+          )}
+        </div>
+      </div>
+    );
+  };
+  
 
 export default Home
